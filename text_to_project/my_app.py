b4e4f566791cfe523737e59e6ae196d2f8ad6f45
kleeding <kawleeding@gmail.com>
2025-01-07 14:07:40 +0000

    updated class names/comments

--- text_to_project/my_app.py
+++ text_to_project/my_app.py
@@ -1,13 +1,12 @@
 from tkinter import Tk, Frame
 from gui.file_explorer import FileExplorer
-from gui.text_editor import TextEditor
+from gui.user_input import UserInput
 from gui.diagram_viewer import DiagramViewer
 
 class MainWindow(Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        # self.columnconfigure(0, weight=4)
         self.columnconfigure((1,2), weight=1, uniform=True)
         self.rowconfigure(0, weight=1)
 
@@ -16,23 +15,24 @@ class MainWindow(Frame):
         self.file_explorer.grid(padx=5, pady=5, row=0, column=0, sticky="news")
 
         # Create text editor
-        self.text_editor = TextEditor(self)
-        self.text_editor.grid(padx=5, pady=5, row=0, column=1, sticky="news")
+        self.user_input = UserInput(self)
+        self.user_input.grid(padx=5, pady=5, row=0, column=1, sticky="news")
 
         # Create diagram viewer
         self.diagram_viewer = DiagramViewer(self)
         self.diagram_viewer.grid(padx=5, pady=5, row=0, column=2, sticky="news")
 
 if __name__ == "__main__":
-
+    # Sort out folder/file information
+    # folder_info = holds a list of (folder_name, (file1, ..., fil2n)) tuples
+    
+    # Create the root window
     root = Tk()
-
-    # Create the main application window
     root.title("Text to Project")
     root.geometry("1400x800")
     root.minsize(width=800, height=500)
  
-    # Create the main application
+    # Create the application
     app = MainWindow(root)
     app.pack(fill="both", expand=True)
 
